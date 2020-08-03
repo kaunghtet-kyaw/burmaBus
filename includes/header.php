523f9abe310ec2bfadd8db66c522a34db4d872cf
40437516 <40437516@live.napier.ac.uk>
2020-08-03 11:30:18 +0630

    added codes for navigation

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<?php ob_start(); ?>
+<?php session_start(); ?>
+<?php include"db.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<title>burmabus.com</title>
+	<!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+	<nav>
+		<div class="topnav"></div>
+		<div class="logo">
+			<a href="#"><img src="img/amarabook.png"></a>
+		</div>
+		<div class="menu">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Flights</a></li>
+				<li><a href="#">Buses</a></li>
+				<li><a href="#">Hotels</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</div>
+	</nav>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,14 +1,25 @@
+<?php include "includes/db.php"; ?>
+<?php include "includes/header.php"; ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>BurmaBus</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<title>burmabus.com</title>
+	<!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
 </head>
 <body>
-	<!-- Start -->
-	<div class="container"> 
+	<div class="container">
 		
-
 	</div>
-
 </body>
 </html>
