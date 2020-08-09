20dd476a5290852056b2a1d181a198ff99ddfa6b
40437516 <40437516@live.napier.ac.uk>
2020-08-09 19:50:49 +0630

    added header file

--- includes/header.php
+++ includes/header.php
@@ -6,11 +6,9 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-	<title>burmabus.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>amarabook</title>
 	<!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
@@ -22,16 +20,14 @@
 	<nav>
 		<div class="topnav"></div>
 		<div class="logo">
-			<a href="#"><img src="img/amarabook.png"></a>
+			<a href="#"><img src="img/amarabook.png" alt="logo"></a>
 		</div>
 		<div class="menu">
-			<ul>
+			<ul class="menu-lists">
 				<li><a href="#">Home</a></li>
 				<li><a href="#">Flights</a></li>
 				<li><a href="#">Buses</a></li>
 				<li><a href="#">Hotels</a></li>
-				<li><a href="#">About Us</a></li>
-				<li><a href="#">Contact Us</a></li>
 			</ul>
 		</div>
 	</nav>
